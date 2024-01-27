21d0fc82a30a7be862950e246f3fc0415b7f4d99
Manishkarke <mk4345437@gmail.com>
2024-01-28 02:23:04 +0545

    Initialized

---
+++ src/ErrorHandler.js
@@ -0,0 +1,82 @@
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const passwordRegex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+// Registration form validator
+export const registrationValidator = (
+  { name, email, password, confirmPassword },
+  setErrors
+) => {
+  // Validate userName
+  if (!name.trim()) {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, name: "Username is required" };
+    });
+  } else {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, name: "" };
+    });
+  }
+
+  // Validate userEmail
+  if (!email.trim()) {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, email: "Email is required" };
+    });
+  } else if (!email.match(emailRegex)) {
+    // error.userEmail = "Email is not valid";
+    setErrors((prevErrors) => {
+      return { ...prevErrors, email: "Email is not valid" };
+    });
+  } else {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, email: "" };
+    });
+  }
+
+  // Validate userPassword
+  if (!password.trim()) {
+    // error.userPassword = "Password is required";
+    setErrors((prevErrors) => {
+      return { ...prevErrors, password: "Password is required" };
+    });
+  } else if (password.trim().length < 8) {
+    // error.userPassword = "Password must be at least 8 character long";
+    setErrors((prevErrors) => {
+      return {
+        ...prevErrors,
+        password: "Password must be at least 8 character long",
+      };
+    });
+  }
+  // else if (!password.match(passwordRegex)) {
+  //   setErrors((prevErrors) => {
+  //     return {
+  //       ...prevErrors,
+  //       password: "Password must have atleast one spacial character",
+  //     };
+  //   });
+  // }
+  else {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, password: "" };
+    });
+  }
+
+  // check if password and confirm password match.
+  if (password !== confirmPassword) {
+    console.log("Password and confirm password are not the same");
+    setErrors((prevErrors) => {
+      return { ...prevErrors, confirmPassword: "Passwords must match" };
+    });
+    console.log(
+      "PASSWORD: " + password + " confirm password: " + confirmPassword
+    );
+  } else if (password === confirmPassword) {
+    console.log("confirmed passwords match");
+    setErrors((prevErrors) => {
+      return { ...prevErrors, confirmPassword: "" };
+    });
+  }
+};
