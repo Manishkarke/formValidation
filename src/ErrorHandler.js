e4b18644ca68fa9adaf911c3dd416fa6d0d9253e
Manishkarke <mk4345437@gmail.com>
2024-01-29 02:06:33 +0545

    Error code

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -2,13 +2,28 @@ const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 // Registration form validator
-export const registrationValidator = ({ email, password }, setErrors) => {
+export const registrationValidator = (
+  { name, email, password, confirmPassword },
+  setErrors
+) => {
+  // Validate userName
+  if (!name.trim()) {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, name: "Username is required" };
+    });
+  } else {
+    setErrors((prevErrors) => {
+      return { ...prevErrors, name: "" };
+    });
+  }
+
   // Validate userEmail
   if (!email.trim()) {
     setErrors((prevErrors) => {
       return { ...prevErrors, email: "Email is required" };
     });
   } else if (!email.match(emailRegex)) {
+    // error.userEmail = "Email is not valid";
     setErrors((prevErrors) => {
       return { ...prevErrors, email: "Email is not valid" };
     });
@@ -20,10 +35,12 @@ export const registrationValidator = ({ email, password }, setErrors) => {
 
   // Validate userPassword
   if (!password.trim()) {
+    // error.userPassword = "Password is required";
     setErrors((prevErrors) => {
       return { ...prevErrors, password: "Password is required" };
     });
   } else if (password.trim().length < 8) {
+    // error.userPassword = "Password must be at least 8 character long";
     setErrors((prevErrors) => {
       return {
         ...prevErrors,
@@ -33,6 +50,22 @@ export const registrationValidator = ({ email, password }, setErrors) => {
   } else {
     setErrors((prevErrors) => {
       return { ...prevErrors, password: "" };
+    });
+  }
+
+  // check if password and confirm password match.
+  if (password !== confirmPassword) {
+    console.log("Password and confirm password are not the same");
+    setErrors((prevErrors) => {
+      return { ...prevErrors, confirmPassword: "Passwords must match" };
+    });
+    console.log(
+      "PASSWORD: " + password + " confirm password: " + confirmPassword
+    );
+  } else if (password === confirmPassword) {
+    console.log("confirmed passwords match");
+    setErrors((prevErrors) => {
+      return { ...prevErrors, confirmPassword: "" };
     });
   }
 };
