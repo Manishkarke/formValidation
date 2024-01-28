13930c0b0c6f2ab964ccb586c1453115e983bbe1
Manishkarke <mk4345437@gmail.com>
2024-01-29 01:22:09 +0545

    managed code

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -2,28 +2,13 @@ const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 // Registration form validator
-export const registrationValidator = (
-  { name, email, password, confirmPassword },
-  setErrors
-) => {
-  // Validate userName
-  if (!name.trim()) {
-    setErrors((prevErrors) => {
-      return { ...prevErrors, name: "Username is required" };
-    });
-  } else {
-    setErrors((prevErrors) => {
-      return { ...prevErrors, name: "" };
-    });
-  }
-
+export const registrationValidator = ({ email, password }, setErrors) => {
   // Validate userEmail
   if (!email.trim()) {
     setErrors((prevErrors) => {
       return { ...prevErrors, email: "Email is required" };
     });
   } else if (!email.match(emailRegex)) {
-    // error.userEmail = "Email is not valid";
     setErrors((prevErrors) => {
       return { ...prevErrors, email: "Email is not valid" };
     });
@@ -35,12 +20,10 @@ export const registrationValidator = (
 
   // Validate userPassword
   if (!password.trim()) {
-    // error.userPassword = "Password is required";
     setErrors((prevErrors) => {
       return { ...prevErrors, password: "Password is required" };
     });
   } else if (password.trim().length < 8) {
-    // error.userPassword = "Password must be at least 8 character long";
     setErrors((prevErrors) => {
       return {
         ...prevErrors,
@@ -50,22 +33,6 @@ export const registrationValidator = (
   } else {
     setErrors((prevErrors) => {
       return { ...prevErrors, password: "" };
-    });
-  }
-
-  // check if password and confirm password match.
-  if (password !== confirmPassword) {
-    console.log("Password and confirm password are not the same");
-    setErrors((prevErrors) => {
-      return { ...prevErrors, confirmPassword: "Passwords must match" };
-    });
-    console.log(
-      "PASSWORD: " + password + " confirm password: " + confirmPassword
-    );
-  } else if (password === confirmPassword) {
-    console.log("confirmed passwords match");
-    setErrors((prevErrors) => {
-      return { ...prevErrors, confirmPassword: "" };
     });
   }
 };
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,42 +1,23 @@
 import React from "react";
 import { registrationValidator } from "./ErrorHandler";
 
-// Tailwind Class Name
-const tailwindClass = {
-  box: "border border-solid rounded-lg shadow flex min-h-full flex-col justify-center align-center px-6 py-12 lg:px-8",
-  inputField:
-    "block w-full p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
-  label: "block text-sm font-medium leading-6 capitalize text-gray-900",
-  links:
-    "font-semibold capitalize ml-1 leading-6 text-indigo-600 hover:text-indigo-500",
-  button:
-    "flex w-full justify-center capitalize rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-  title:
-    " text-center text-2xl font-bold leading-9 tracking-tight text-gray-900",
-  error: "text-red-600 capitalize text-sm",
-};
-
 function Register() {
   // Component form state states
   const [formData, setFormData] = React.useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
 
-  // For FOrm validation
+  // For Form validation
   const [isFormSubmitted, setFormSubmitted] = React.useState(false); // to check if submit button is clicked
   const [errors, setErrors] = React.useState({}); // to store error states
-  const [isFormValid, setIsFormValid] = React.useState(true); // to check if form is valid
+  let isFormValid = true; // to check if form is valid
 
   React.useEffect(() => {
     for (const error in errors) {
       if (errors[error]) {
-        setIsFormValid(false); // if There is any error then
+        isFormValid = false; // if There is any error then
         break;
-      } else {
-        setIsFormValid(true);
       }
     }
     console.log(
@@ -70,114 +51,45 @@ function Register() {
   };
 
   return (
-    <>
-      <div className="main-box">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className={tailwindClass.title}>Register new account</h2>
+    <div className="main-box">
+      <h2>Register new account</h2>
+
+      <form method="POST" onSubmit={formSubmitHandler}>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <div>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={inputFieldChangeHandler}
+              autoComplete="email"
+            />
+          </div>
+          {errors.email && <span>{errors.email}</span>}
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            className="space-y-6"
-            method="POST"
-            onSubmit={formSubmitHandler}
-          >
-            <div>
-              <label htmlFor="fullName" className={tailwindClass.label}>
-                full name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="fullName"
-                  name="name"
-                  type="text"
-                  value={formData.name}
-                  onChange={inputFieldChangeHandler}
-                  autoComplete="fullName"
-                  className={tailwindClass.inputField}
-                />
-              </div>
-              {errors.name && (
-                <span className={tailwindClass.error}>{errors.name}</span>
-              )}
-            </div>
-            <div>
-              <label htmlFor="email" className={tailwindClass.label}>
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  value={formData.email}
-                  onChange={inputFieldChangeHandler}
-                  autoComplete="email"
-                  className={tailwindClass.inputField}
-                />
-              </div>
-              {errors.email && (
-                <span className={tailwindClass.error}>{errors.email}</span>
-              )}
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className={tailwindClass.label}>
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  value={formData.password}
-                  onChange={inputFieldChangeHandler}
-                  autoComplete="current-password"
-                  className={tailwindClass.inputField}
-                />
-              </div>
-              {errors.password && (
-                <span className={tailwindClass.error}>{errors.password}</span>
-              )}
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="confirmPassword"
-                  className={tailwindClass.label}
-                >
-                  confirm password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  value={formData.confirmPassword}
-                  onChange={inputFieldChangeHandler}
-                  autoComplete="current-password"
-                  className={tailwindClass.inputField}
-                />
-              </div>
-              {errors.confirmPassword && (
-                <span className={tailwindClass.error}>
-                  {errors.confirmPassword}
-                </span>
-              )}
-            </div>
-
-            <div>
-              <button type="submit" className={tailwindClass.button}>
-                sign up
-              </button>
-            </div>
-          </form>
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={inputFieldChangeHandler}
+              autoComplete="current-password"
+            />
+          </div>
+          {errors.password && <span>{errors.password}</span>}
         </div>
-      </div>
-    </>
+
+        <button type="submit">sign up</button>
+      </form>
+    </div>
   );
 }
 
